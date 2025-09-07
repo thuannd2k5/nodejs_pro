0ed937e8b4941ca41c5b9da682ca26d35b1be53d
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-09-07 14:39:11 +0700

    hêlo world

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/thuan', (req, res) => {
+    res.send('nguyenducthuan')
+})
+
+app.listen(8080, () => {
+    console.log(`my app is running on port : ${PORT}`);
+})
