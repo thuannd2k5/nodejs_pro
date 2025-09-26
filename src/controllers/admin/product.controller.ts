45831837f7958413742ac6a7f02bbe44dff2e3ef
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-09-27 00:05:24 +0700

    xử lý thành công validate data

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -8,17 +8,33 @@ const getAdminProductPage = async (req: Request, res: Response) => {
 
 
 const getCreateAdminProductPage = async (req: Request, res: Response) => {
-    return res.render("admin/product/create")
+    const errors = [];
+    const oldData = {
+        name: "",
+        price: "",
+        detailDesc: "",
+        shortDesc: "",
+        quantity: "",
+
+    }
+    return res.render("admin/product/create", {
+        errors, oldData
+    })
 }
 
 const postCreateAdminProduct = async (req: Request, res: Response) => {
-    const { name } = req.body as TProductSchema;
-    try {
-        const result = ProductSchema.parse(req.body);
-        console.log("run oke ", result)
-    } catch (error) {
-        console.log(error)
+    const { name, price, detailDesc, shortDesc, quantity } = req.body as TProductSchema;
+    const validate = ProductSchema.safeParse(req.body);
+    const oldData = { name, price, detailDesc, shortDesc, quantity }
+    if (!validate.success) {
+        //error
+        const errorZod = validate.error.issues;
+        const errors = errorZod?.map(item => `${item.message} (${item.path[0]})`)
+        return res.render("admin/product/create", {
+            errors, oldData
+        })
     }
+
     return res.redirect("/admin/product");
 }
 
