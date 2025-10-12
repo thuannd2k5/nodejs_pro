c5ba4649f4d8325910bf1f0337dc214f84576a01
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-12 19:32:53 +0700

    cors

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,15 @@ import session from 'express-session';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from '@prisma/client';
 import apiRoutes from 'routes/api';
+import cors from 'cors'
+
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+//config cors
+app.use(cors())
+
 
 //config viewengine
 app.set('view engine', 'ejs');
